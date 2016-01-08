32a7715a121ab8ef31be9c2436e1be6ac3679b77
Zero2848 <18187@jcpstudents.org>
2016-01-08 16:50:48 -0600

    Made a branch

---
+++ 2014TestRobot/src/org/usfirst/frc/team2850/robot/SparkyDriveHelper.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team2850.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
+
+public class SparkyDriveHelper {
+
+	
+	public static void arcadeDrive( Joystick xbox, RobotDrive drive,Victor motor1, Victor motor2){
+		
+		drive.arcadeDrive(xbox.getRawAxis(1), xbox.getRawAxis(4)); 
+        	
+        
+	}
+}
