9bdd76a795faaadeacbea3b0d35fdbffcaab4e79
tkddu <tkddu@10.4.2.80>
2016-01-08 16:31:16 -0600

    Initialized the drivetrain Initialized the shifter pneumatics Initialized the other motors

--- 2014TestRobot/src/org/usfirst/frc/team2850/robot/Robot.java
+++ 2014TestRobot/src/org/usfirst/frc/team2850/robot/Robot.java
@@ -2,6 +2,10 @@
 package org.usfirst.frc.team2850.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,7 +19,34 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+	public Joystick xbox1;
+	public Joystick xbox2;
+	
+	public RobotDrive drivetrain;
+	
+	public Solenoid shifter1;
+	public Solenoid shifter2;
+	
+	public Victor motor1;
+	public Victor motor2;
+	public Victor motor3;
+	public Victor motor4;
+	public Victor motor5;
+	
     public void robotInit() {
+    	xbox1 = new Joystick(0);
+    	xbox2 = new Joystick(1);
+    	
+    	drivetrain = new RobotDrive(new Victor(0), new Victor(1));
+    	
+    	shifter1 = new Solenoid(0);
+    	shifter2 = new Solenoid(1);
+    	
+    	motor1 = new Victor(2);
+    	motor2 = new Victor(3);
+    	motor3 = new Victor(4);
+    	motor4 = new Victor(5);
+    	motor5 = new Victor(6);
 
     }
 
